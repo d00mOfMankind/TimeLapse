3a576256fd97dc405771b37e16a1ecde9f2710ce
Dom <dominic.routley@devopsguys.com>
2018-02-05 15:01:57 +0000

    update ping test

--- dev/pingtest.sh
+++ dev/pingtest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-code=$(ssh -i ./bin/pi-ssh-key pi@raspberrypi-bane echo "0")
+code=$(ssh -i ./bin/pi-ssh-key pi@raspberrypi-bane touch test.txt)
 if [ "$code" == "0" ]
 then
-  echo "No connection"
+  echo "Code == 0"
 else
-  echo "Connection"
+  echo "Code != 0"
 fi
 echo $code
