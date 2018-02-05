d374e995ee48b6c438d17db79712bf1715faf677
Dom <dominic.routley@devopsguys.com>
2018-02-05 10:44:22 +0000

    added ssh key functionality

--- TimeLapseSetup.sh
+++ TimeLapseSetup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 function usage() {
-	echo "Usage: $(basename ${0}) --(viewtest {unique name} | start {unique name} {time interval} {number of images})"
+	echo "Usage: $(basename ${0}) --(viewtest {unique name} | start {unique name} {time interval} {number of images}"
+	echo "                        --key {ssh key name})"
 	echo ""
-	echo " --viewtest   -v      - Get a single image from the remote camera to test the view."
-	echo "                        'unique name' referes to the name suffix of the Raspberry Pi"
+	echo "   Example:   $(basename ${0}) --viewtest maleficent --key my-private-key"
+	echo ""
+	echo " --viewtest           - Get a single image from the remote camera to test the view."
+	echo "                        'unique name' referes to the name suffix of the Raspberry Pi."
+	echo " --start              - Start the timelapse camera."
+	echo "                        'unique name' referes to the name suffix of the Raspberry Pi."
+	echo "                        'time interval' refers to the time the camera will wait between images."
+	echo "                        'number of images' refers to the number of images that you want the camera to take."
+	echo " --key                - Provides the ssh key for the connection."
 	echo ""
 	exit -1
 }
@@ -58,6 +66,8 @@ function setup(){
 UNIQUE_NAME=""
 TIME_INTERVAL=""
 NUMBER_OF_IMAGES=""
+KEY_NAME=""
+TOGGLE=""
 
 if [[ $# -eq 0 ]]
 then
@@ -65,37 +75,65 @@ then
   exit 1
 fi
 
-case "${1}" in
-  -h|--help)
-		usage;;
-	-v|--viewtest)
-		if [ -z "${2}" ]
-		then
-		  echo "ERROR: (--viewtest|-v) Target not defined."
-		  exit 1
-		else
-			UNIQUE_NAME=${2}
-			get_static_image $UNIQUE_NAME
-		fi
-	-s|--start)
-		if [ -z "${2}" ]
-		then
-			echo "ERROR: (--start|-s) Target not defined."
+while [[ $# -gt 0 ]]; do
+	case "${1}" in
+	  -h|--help)
+			usage;;
+		--viewtest)
+			if [ -z "${2}" ]
+			then
+			  echo "ERROR: (--viewtest) Target not defined."
+			  exit 1
+			else
+				UNIQUE_NAME=${2}
+				TOGGLE="view"
+			fi
+			shift 2;;
+		--start)
+			if [ -z "${2}" ]
+			then
+				echo "ERROR: (--start) Target not defined."
+				exit 1
+			elif [ -z "${3}" ]
+			then
+				echo "ERROR: (--start) Time interval not defined."
+				exit 1
+			elif [ -z "${4}" ]
+			then
+				echo "ERROR: (--start) Number of images not defined."
+				exit 1
+			else
+				UNIQUE_NAME=${2}
+				TIME_INTERVAL=${3}
+				NUMBER_OF_IMAGES=${4}
+				TOGGLE="start"
+			fi
+			shift 4;;
+		--key)
+			if [ -z "${2}" ]
+			then
+				echo "ERROR: (--key) ssh key not defined."
+				exit 1
+			elif [ -f "./bin/${2}" ]
+      then
+        echo "ERROR: Key: ./bin/${2} does not exist."
+        exit 1
+      else
+      	KEY_NAME=${2}
+      shift 2;;
+		*)
+			echo "FATAL: Unknown command-line argument or enviroment: ${1}"
 			exit 1
-		elif [ -z "${3}" ]
-		then
-			echo "ERROR: (--start|-s) Time interval not defined."
-			exit 1
-		elif [ -z "${4}" ]
-		then
-			echo "ERROR: (--start|-s) Number of images not defined."
-			exit 1
-		else
-			UNIQUE_NAME=${2}
-			TIME_INTERVAL=${3}
-			NUMBER_OF_IMAGES=${4}
-			setup $UNIQUE_NAME TIME_INTERVAL NUMBER_OF_IMAGES
-		fi
-	*)
-		echo "FATAL: Unknown command-line argument or enviroment: ${1}"
-esac
+	esac
+done
+
+if [ "$TOGGLE" == "start" ]
+then
+	setup $UNIQUE_NAME TIME_INTERVAL NUMBER_OF_IMAGES
+elif [ "$TOGGLE" == "view" ]
+then
+	get_static_image $UNIQUE_NAME
+else
+	echo "FATAL: No mode selected."
+	usage
+fi
