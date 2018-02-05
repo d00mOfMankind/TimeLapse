00a6aa74dd24b38cfa672ce7f68effdcd812cd4c
Dom <dominic.routley@devopsguys.com>
2018-02-05 15:05:02 +0000

    foo

--- dev/pingtest.sh
+++ dev/pingtest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-code=$(ssh -i ~/projects/TimeLapse/bin/pi-ssh-key pi@raspberrypi-bane touch test.txt)
+code=$(ssh -i ~/projects/TimeLapse/bin/pi-ssh-key pi@raspberrypi-bane touch echo "foo")
 if [ "$code" == "0" ]
 then
   echo "Code == 0"
