811f21eccb5167377ad80150f1ae359a86f7a8ea
Dom <dominic.routley@devopsguys.com>
2018-02-05 13:33:56 +0000

    overwrite test

---
+++ dev/two.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo -n "[>.........]"
+sleep 0.2
+echo -n "[=>........]"
+sleep 0.2
+echo -n "[==>.......]"
+sleep 0.2
+echo -n "[===>......]"
+sleep 0.2
+echo -n "[====>.....]"
+sleep 0.2
+echo -n "[=====>....]"
+sleep 0.2
+echo -n "[======>...]"
+sleep 0.2
+echo -n "[=======>..]"
+sleep 0.2
+echo -n "[========>.]"
+sleep 0.2
+echo -n "[=========>]"
+sleep 0.2
+echo -n "[==========]"
