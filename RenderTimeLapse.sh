7559bdafbaa32df59ea93508c48569fd33928e32
d00mOfMankind <dominic.routley@gmail.com>
2018-02-08 11:47:22 +0000

    RenderTimeLapse now a stripped version of TimeLapseSetup with no options other than download and render

--- RenderTimeLapse.sh
+++ RenderTimeLapse.sh
@@ -1,23 +1,76 @@
 #!/usr/bin/env bash
 
 function usage() {
-  echo "Usage: $(basename ${0}) --[ remote {unique name} {ssh key name}"
+  echo "Usage: $(basename ${0}) --[ remote {unique name}"
+  echo "                          | fetch {unique name}"
   echo "                          | local {path}]"
+  echo "                            --key {ssh key name}"
   echo "                            --help"
   echo ""
-  echo "   Examples:   $(basename ${0}) --remote maleficent my-private-key"
+  echo "   Examples:   $(basename ${0}) --fetch maleficent --key my-private-key"
   echo "               $(basename ${0}) --local images"
   echo ""
-  echo " --remote     -r      - Fetch timelapse images from Raspberry Pi."
+  echo " --remote             - Fetch timelapse images from Raspberry Pi."
   echo "                        'unique name' referes to the name suffix of the Raspberry Pi."
   echo "                        remote assumes that the remote path is ~/TimeLapse_images"
   echo "                        'ssh key name' is the name of the ssh private key that the program."
   echo "                        will attempt to connect with. This should be placed in ./bin"
-  echo " --local      -l      - Use locally stored timelapse images."
+  echo " --local              - Use locally stored timelapse images."
+  echo "                        'folder' is the name of the local folder (path from same working directory) to get images from."
+  echo " --fetch              - Fetch timelapse images form Raspberry Pi, and save to folder."
+  echo "                        Saves to folder ./images and will overwrite any images in ./images"
+  echo "                        to prevent this rename the folder to something else. --fetch will create a new folder called ./images to save too."
+  echo "                        'unique name' referes to the name suffix of the Raspberry Pi."
+  echo ""
+  echo " --key       -k/-i    - Provides the ssh key for the connection."
   echo ""
   echo " --help       -h      - View this page."
   echo ""
   exit -1
+}
+
+# var1 = sshkey, var2 = hostname
+function validation() {
+  host=$2
+  echo "Hostname: $host"
+  key=$1
+  echo "ssh key : $key"
+
+  echo ""
+
+  #key name check
+  if [ "$key" == "-" ]
+  then
+    echo "ERROR: (--key) ssh key not defined."
+    exit 1
+  fi
+
+  #hostname ping test
+  ping -c 1 raspberrypi-$host > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    echo "ERROR: Host raspberrypi-$host unreachable/offline."
+    exit 1
+  else
+    echo "INFO: Host raspberrypi-$host online."
+  fi
+
+  echo ""
+
+  #connection test
+  extcode=$(ssh -i ./bin/$key pi@raspberrypi-$host echo "ssh connection test")
+  if [ "$extcode" == "ssh connection test" ]
+  then
+    echo "INFO: Connection to raspberrypi-$host established."
+  else
+    echo "ERROR: Unable to establish ssh connection to raspberrypi-$host"
+    echo "     : Most probable cause, wrong ssh key used."
+    echo "     : Make sure your key is in ./bin"
+    exit 1
+  fi
+
+  echo ""
+
 }
 
 function fetch_images() {
@@ -31,7 +84,25 @@ function fetch_images() {
     exit 1
   fi
 
-  scp -i ./bin/$2 -r pi@raspberrypi-$1:~/TimeLapse_images ./images
+  if [ -d images ]
+  then
+    echo "INFO: Default images folder exists."
+    echo "    : Continuing with this download will remove all currently stored images in ./images."
+    read -p "OPTION: Do you want to continue with this download? y/n: " rmimages
+
+    if [ "$rmimages" == "y" ] || [ "$rmimages" == "Y" ] || [ "$rmimages" == "yes" ] || [ "$rmimages" == "Yes" ]
+    then
+      echo "STATUS: Removing local ./images..."
+      rm -r images || exit 1
+    else
+      echo "STATUS: Cancelling download..."
+      exit 1
+    fi
+  fi
+
+  echo "STATUS: Saving images to folder ./images ..."
+  scp -r -i ./bin/$2 pi@raspberrypi-$1:~/TimeLapse_images ./images
+  echo "INFO: $NUMBER files downloaded..."
 
 }
 
@@ -50,12 +121,12 @@ function render() {
   #Check if ffmpeg exists
   if [ ! -f ffmpeg ]
   then
-  	echo "ERROR: ffmpeg does not exist in local directory."
-  	echo "     : Download ffmpeg from either"
+    echo "ERROR: ffmpeg does not exist in local directory."
+    echo "     : Download ffmpeg from either"
     echo "     : https://www.ffmpeg.org/Download/ or http://ffbinaries.com/downloads"
     echo "     : and extract the ffmpeg file to local location."
     echo "     : The images will not be deleted."
-  	exit 1
+    exit 1
   fi
 
 
@@ -74,17 +145,18 @@ function render() {
   #Removing unneeded files (possibly)
   if [ "$remove_option" == "y" ] || [ "$remove_option" == "Y" ] || [ "$remove_option" == "yes" ] || [ "$remove_option" == "Yes" ]
   then
-  	echo "STATUS: Cleaning up unneeded files..."
-  	rm -r $1
+    echo "STATUS: Cleaning up unneeded files..."
+    rm -r $1
   fi
 
   echo "INFO: Render complete."
 
 }
 
-IMAGE_PATH=""
-UNIQUE_NAME=""
-KEY_NAME=""
+IMAGE_PATH="-"
+UNIQUE_NAME="-"
+KEY_NAME="-"
+TOGGLE="-"
 
 if [[ $# -eq 0 ]]
 then
@@ -92,51 +164,87 @@ then
   exit 1
 fi
 
-#this is a redundant while loop... But will remain as example
-
-case "${1}" in
-  -h|--help)
-    usage
-    ;;
-  --remote|-r)
-		if [ -z "${2}" ]
-		then
-			echo "ERROR: (--remote) Target not defined."
-			exit 1
-    elif [ -z "${3}" ]
-    then
-      echo "ERROR: (--remote) ssh key not defined."
-      exit 1
-		else
-      if [ ! -f "./bin/${3}" ]
-      then
-        echo "ERROR: Key: ./bin/${3} does not exist."
-      fi
-			UNIQUE_NAME=${2}
-      KEY_NAME=${3}
-      IMAGE_PATH="./images"
-			fetch_images $UNIQUE_NAME $KEY_NAME
-      render $IMAGE_PATH
-    fi
-    ;;
-  --local|-l)
-    if [ -z "${2}" ]
-    then
-      echo "ERROR: (--local) Path not defined."
-      exit 1
-    else
-      if [ -d ${2} ]
-      then
-        IMAGE_PATH=${2}
-        render $IMAGE_PATH
+while [[ $# -gt 0 ]]; do
+  case "${1}" in
+    -h|--help)
+      usage
+      ;;
+    --remote)
+  		if [ -z "${2}" ] || [[ "${2}" == -* ]]
+  		then
+  			echo "ERROR: (--remote) Target not defined."
+  			exit 1
       else
-        echo "ERROR: Path: ${2} does not exist."
-        exit 1
+  			UNIQUE_NAME=${2}
+        IMAGE_PATH="./images"
+        TOGGLE="remote"
       fi
-    fi
-    ;;
-  *)
-    echo "FATAL: Unknown command-line argument or environment: ${1}"
-    exit 1
-    ;;
-esac
+      ;;
+    --local)
+      if [ -z "${2}" ] || [[ "${2}" == -* ]]
+      then
+        echo "ERROR: (--local) Path not defined."
+        exit 1
+      else
+        if [ -d ${2} ]
+        then
+          IMAGE_PATH=${2}
+          TOGGLE="local"
+        else
+          echo "ERROR: Path: ${2} does not exist."
+          exit 1
+        fi
+      fi
+      shift 2
+      ;;
+    --fetch)
+      if [ -z "${2}" ] || [[ "${2}" == -* ]]
+      then
+        echo "ERROR: (--fetch) Target not defined."
+        exit 1
+      else
+        UNIQUE_NAME=${2}
+        TOGGLE="fetch"
+      fi
+      shift 2
+      ;;
+    --key|-k|-i)
+      if [ -z "${2}" ] || [[ "${2}" == -* ]]
+      then
+        echo "ERROR: (--key) ssh key not defined."
+        exit 1
+      elif [ ! -f "./bin/${2}" ]
+      then
+        echo "ERROR: Key: ./bin/${2} does not exist."
+        exit 1
+      else
+        KEY_NAME=${2}
+      fi
+      shift 2
+      ;;
+    *)
+      echo "FATAL: Unknown command-line argument or environment: ${1}"
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$TOGGLE" == "remote" ]
+then
+  validation $KEY_NAME $UNIQUE_NAME
+  fetch_images $UNIQUE_NAME $KEY_NAME
+  render $IMAGE_PATH
+
+elif [ "$TOGGLE" == "local" ]
+then
+  render $IMAGE_PATH
+
+elif [ "$TOGGLE" == "fetch" ]
+then
+  validation $KEY_NAME $UNIQUE_NAME
+  fetch_images $UNIQUE_NAME $KEY_NAME
+
+else
+  echo "FATAL: No mode selected."
+  usage
+fi
